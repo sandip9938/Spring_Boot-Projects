c6b92d94ed840f56224ed7b3fd729de9482d6c99
sandi <sandi@192.168.1.6>
2024-11-08 23:16:17 +0530

    Intial Commit

---
+++ SpringBootProject03-MiniProject-Insert_Select_Employee/src/main/java/com/sp/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.sp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.sp.dao.IEmployeeDao;
+import com.sp.model.Employee;
+
+@Service("empService")
+public class EmployeeService implements IEmployeeService {
+
+	@Autowired
+	private IEmployeeDao dao;
+
+	@Override
+	public List<Employee> fetchallEmployessByDesg(String desg1, String desg2) throws Exception {
+		List<Employee> list = dao.getEmployeeDesg(desg1, desg2);
+		return list;
+	}
+
+	@Override
+	public String registerEmployee(Employee emp) throws Exception {
+		int result = dao.insertEmployee(emp);
+		return result == 0 ? "Employee Not Registered" : "Employee Registered";
+	}
+
+}
